a51f40a93086b1cb10348154b68c437f410e6a49
praneeth reddy bilakanti <praneethreddy8498995302@gmail.com>
2020-09-22 15:26:33 +0530

    slueth

---
+++ spring-sleuth4/src/main/java/com/praneeth/SpringSleuth1Application.java
@@ -0,0 +1,47 @@
+package com.praneeth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+public class SpringSleuth1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSleuth1Application.class, args);
+	}
+
+}
+@RestController
+class Microservice1Controller {
+
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+	@GetMapping(value = "/microservice4")
+	public String method4() {
+		LOG.info("Inside method4");
+		return "Hello World JavaInUse";
+	}
+}
